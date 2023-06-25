33b1f405fd75305d566669662fdc2a442a90a708
mainakdasgupta2702 <107871967+greenshark27022002@users.noreply.github.com>
2023-06-25 20:08:19 +0530

    Submitted

---
+++ main.py
@@ -0,0 +1,9 @@
+print ('Give customer to seasoned sales associate.')
+print('Give cstomer to mid level sales associate.')
+print('Please return when you\'d like to predict a customer sale.')
+print('Enjoy your day!')
+print ('Welcome to the Sales predictor!')
+a = raw_input('Are you looking to predict a customer outcome? ')
+response = a.lower()
+while response == 'yes':
+    print ('Thank you for using Sales Predictor 1.0 beta!')
